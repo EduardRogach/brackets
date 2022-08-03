c45ed13080ffaef4d7dddf1bd3769072554baa79
Eduard Rogach <eduard.rogach@gmail.com>
2022-08-03 08:00:02 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+   const brackets = Object.fromEntries(bracketsConfig);
+   
+   const stack = [];
+
+   for (let i = 0; i < str.length; i++) {
+    if (stack.length === 0) {
+		stack.push(str[i]);
+  		} else { 
+  		if (str[i] == brackets[stack[stack.length - 1]]) {
+				stack.pop() 
+				} else {
+					stack.push(str[i])
+				}
+ 		}
+  }  
+return !stack.length;
 }
